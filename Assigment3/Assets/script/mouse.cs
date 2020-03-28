cc814cb37bf0920b4bf5c7267e12a8f98dbc2525
Amonpan-suru <amonpan.suru@mail.kmutt.ac.th>
2020-03-28 17:10:03 +0700

    frist
    1

---
+++ Assigment3/Assets/script/mouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse : MonoBehaviour
+{
+    
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosWorld = Input.mousePosition;
+        mousePosWorld.z = transform.position.z - Camera.main.transform.position.z;
+        mousePosWorld = Camera.main.ScreenToViewportPoint(mousePosWorld);
+
+        transform.position = new Vector3(mousePosWorld.x, mousePosWorld.y, transform.position.z);
+        //Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(gameObject.name);
+    }
+/*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals())
+        {
+            Debug.Log("win");
+        }
+    }
+    */
+
+}
